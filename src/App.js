d021a136361ed125345f7f25bda75b4061e05043
Cody Denniston <dennistonc4817@gapps.bcsds.org>
2020-02-28 11:25:18 -0600

    about to start styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from "react";
 import Header from "./components/Header.js";
+import CharacterCard from "./components/CharacterCard"
+import CharacterList from "./components/CharacterList"
+import LocationCard from "./components/LocationCard"
+import LocationsList from "./components/LocationsList"
+import SearchForm from "./components/SearchForm"
+import WelcomePage from "./components/WelcomePage"
+import {
+  Link,
+  Switch,
+  Route
+} from "react-router-dom";
 
 
 export default function App() {
+  
   return (
     <main>
-      <Header />
+      <Header/>
+      <Switch>
+        <Route exact path="/">
+        <WelcomePage/>
+        </Route>
+         
+        <Route exact path="/CharacterList">
+          <CharacterList />
+        </Route>
+      </Switch>
+     
     </main>
   );
 }
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,20 @@
 import React from "react";
+import {
+  Card, CardImg, CardText, CardBody, CardLink,
+  CardTitle, CardSubtitle
+} from 'reactstrap';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return (
+    <div>
+      <Card>
+        <CardBody>
+          <CardTitle>Name: {props.name}</CardTitle>
+          <CardSubtitle>Status: {props.status}</CardSubtitle>
+          <CardSubtitle>Species: {props.species}</CardSubtitle>
+        </CardBody>
+        <img width="100%" src= {props.img} alt="Character Card" />
+      </Card>
+    </div>
+  );
 }
